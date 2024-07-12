478914c9251ef060fed01ef71edc90e9cc036d42
Arjun Goel <arjungoel1995@gmail.com>
2024-07-12 15:27:54 +0530

    modified the workflow step

--- add_numbers.py
+++ add_numbers.py
@@ -1,7 +1,7 @@
 # Following standards
-def add_numbers(val1, val2):
+def add_numbers(val1,val2):
     """
     Print sum of two numbers
     """
-    total = val1 + val2
+    total=val1+val2
     print(total)
